a6b1c1c278861fd41180637f9f92626b1399bd1e
githa12 <159309793+githa12@users.noreply.github.com>
2024-03-21 00:38:56 +0530

    Update multipleinheritance.java

--- multipleinheritance.java
+++ multipleinheritance.java
@@ -1,25 +1,32 @@
-//Multiple inheritance
-class Mammal{
-	void M(){
-		System.out.println("mammals are warm blooded");
-	}
+interface Mammal {
+    void M();
 }
-class Rodents{
-	void R(){
-		System.out.println("rodents are small creatures");
-	}
+
+interface Rodents {
+    void R();
 }
-class Rats extends Mammal , Rodents{
-	 void r(){
-		System.out.println("rats belong to rodents of mammals");
-	}
-	public static void main(String args[]){
-		Rats obj=new Rats();
-		obj.M();
-		obj.R();
-		obj.r();
+
+class Rats implements Mammal, Rodents {
+    public void M() {
+        System.out.println("mammals are warm blooded");
+    }
+
+    public void R() {
+        System.out.println("rodents are small creatures");
+    }
+
+    void r() {
+        System.out.println("rats belong to rodents of mammals");
+    }
+
+    public static void main(String args[]) {
+        Rats obj = new Rats();
+        obj.M();
+        obj.R();
+        obj.r();
+    }
 }
-	}
+
 
 
 
