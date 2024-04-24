37350a47bccc1dcb254d6ca8bd5e4dff211c955c
githa12 <159309793+githa12@users.noreply.github.com>
2024-04-24 13:39:41 +0530

    Update pureabstraction.java

--- pureabstraction.java
+++ pureabstraction.java
@@ -1,25 +1,40 @@
 import java.util.Scanner;
-class Overriding
-{ 
-    void sum(int a,int b){
-        System.out.println("The sum of the two given numbers:"+(a+b));
-    }
-    void sum(float a,float b)
-    {
-        System.out.println("overriden function :"+(a+b));
-    }
-public static void main(String args[]){
-    Overriding obj=new Overriding();
-    Scanner input=new Scanner(System.in);
-    float x,y;
-    int p,q;
-    System.out.println("enter any two integers:");
-    p=input.nextInt();
-    q=input.nextInt();
-    
-    obj.sum(p,q);
-        System.out.println("enter any float values:");
-    x=input.nextFloat();
-    y=input.nextFloat();
-    obj.sum(x,y);
+interface Polygon{
+    float area(float a,float b);
+    float perimeter(float a,float b,float c);
 }
+class Rectangle implements Polygon{
+        public float area(float a,float b){
+            return a*b;
+        }
+        public float perimeter(float a,float b,float c)
+        {
+            System.out.println(2*(a+b));
+            return 2*(a+b);
+        }
+}
+class Triangle implements Polygon{
+        public float area(float a,float b){
+            System.out.println((0.5f)*(a*b));
+            return 0.5f*(a*b);
+        }
+        public float perimeter(float a,float b,float c)
+        {
+           System.out.println(a+b+c);
+           return a+b+c;
+        }
+}
+public class Pure{
+    public static void main(String args[]){
+        Rectangle rec=new Rectangle();
+        Triangle tri=new Triangle();
+        rec.perimeter(1,2,0);
+        tri.perimeter(9,7,6);
+        float result1,result2;
+        result1=rec.area(2,5);
+        result2=tri.area(8,8);
+        System.out.println("the area of rectangle ="+result1);
+        System.out.println("the area of triangle ="+result2);
+    }
+}
+
